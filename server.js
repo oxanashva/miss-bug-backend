88886f087fa7533a7e762303ec7b9521bb22aa4e
oxanashva <oxanashav@gmail.com>
2025-11-20 14:47:32 +0200

    chore: fix server setup for prod and dev, ensure Express handles client-side routes on reload

--- server.js
+++ server.js
@@ -9,20 +9,23 @@ const app = express()
 
 //* ------------------- Config -------------------
 
-const corsOptions = {
-    origin: [
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:5174",
-        "http://localhost:5174"
-    ],
-    credentials: true
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client"))
+} else {
+    const corsOptions = {
+        origin: [
+            "http://127.0.0.1:5173",
+            "http://localhost:5173",
+            "http://127.0.0.1:5174",
+            "http://localhost:5174"
+        ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
 
-app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static("public"))
 app.set("query parser", "extended")
 
 import { bugRoutes } from './api/bug/bug.routes.js'
@@ -33,6 +36,10 @@ app.use("/api/user", userRoutes)
 
 import { authRoutes } from "./api/auth/auth.routes.js"
 app.use("/api/auth", authRoutes)
+
+app.get('/*all', (req, res) => {
+    res.sendFile("public/index.html", { root: "." })
+})
 
 const port = process.env.PORT
 app.listen(port, () => {
