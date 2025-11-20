e61b3a828303ec5e4da8aa4879a903fee4657730
oxanashva <oxanashav@gmail.com>
2025-11-20 15:12:22 +0200

    fix: serve client files from the public folder

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 //* ------------------- Config -------------------
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client"))
+    app.use(express.static("public"))
 } else {
     const corsOptions = {
         origin: [
